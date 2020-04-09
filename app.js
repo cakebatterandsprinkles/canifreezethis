205a18a44111e93dc026ea4f933856377508045e
yagmurcetintas <yagmurcetin@gmail.com>
2020-04-09 10:44:03 -0400

    added mongoose connection to database and edited scripts

--- app.js
+++ app.js
@@ -8,5 +8,15 @@ require('dotenv').config({
   path: 'variables.env'
 });
 const PORT = process.env.PORT || 5000;
+const DATABASE = process.env.DATABASE;
+
+mongoose.connect(DATABASE, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', error => {
+  console.log(`Mongoose connection error: ${error.message}`)
+})
 
 app.listen(PORT, () => console.log(`Backend server started on port: ${PORT}`));
